d67b331d56436362112d8a5bef4e1b9a167ada1d
Your Name <you@example.com>
2018-08-25 11:39:01 +0300

    Initial Commit

---
+++ FlooidRealm/FlooidRealm.h
@@ -0,0 +1,19 @@
+//
+//  FlooidRealm.h
+//  FlooidRealm
+//
+//  Created by Martin Lalev on 25.08.18.
+//  Copyright © 2018 Martin Lalev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlooidRealm.
+FOUNDATION_EXPORT double FlooidRealmVersionNumber;
+
+//! Project version string for FlooidRealm.
+FOUNDATION_EXPORT const unsigned char FlooidRealmVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlooidRealm/PublicHeader.h>
+
+
