94f9ee2e46529115682f442a8243f07ff8c94c97
Martin Lalev <martin.laleff@gmail.com>
2019-12-02 15:09:57 +0000

    Update callbacks now include the object itself along with the changes;

--- FlooidRealm/ObjectObserver.swift
+++ FlooidRealm/ObjectObserver.swift
@@ -21,13 +21,13 @@ extension DataObjectProtocol where Self: Object {
         })
     }
     
-    public static func updateObserver(for id: String, in context:RealmContext, callback: @escaping ([String: (old: Any?, new: Any?)]) -> Void) -> AnyObject? {
+    public static func updateObserver(for id: String, in context:RealmContext, callback: @escaping (Self, [String: (old: Any?, new: Any?)]) -> Void) -> AnyObject? {
         return Self.object(forID: id, in: context)?.updateObserver(callback: callback)
     }
-    public func updateObserver(callback: @escaping ([String: (old: Any?, new: Any?)]) -> Void) -> AnyObject {
+    public func updateObserver(callback: @escaping (Self, [String: (old: Any?, new: Any?)]) -> Void) -> AnyObject {
         return self.observe({ (objectChange) in
             guard case .change(let propertyChanged) = objectChange else { return }
-            callback(propertyChanged.reduce(into: [:]) { $0[$1.name] = (old:$1.oldValue,new:$1.newValue) })
+            callback(self, propertyChanged.reduce(into: [:]) { $0[$1.name] = (old:$1.oldValue,new:$1.newValue) })
         })
     }
 }
